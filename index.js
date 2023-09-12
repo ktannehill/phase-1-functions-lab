bf2b6b3760f88bdd4196491867c089a2c1c1edc8
Kat Tannehill <katetannehill@gmail.com>
2023-09-11 20:14:39 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+const scuberHqBlock = 42;
+
+function distanceFromHqInBlocks(passengerBlock) {
+    if(passengerBlock >= 42) {
+        return passengerBlock - scuberHqBlock;
+    } else {
+        return scuberHqBlock - passengerBlock;
+    }
+};
+
+function distanceFromHqInFeet(passengerBlock) {
+    return distanceFromHqInBlocks(passengerBlock) * 264;
+};
+
+function distanceTravelledInFeet(start, destination) {
+    if(start > destination) {
+        return 264 * (start - destination);
+    }
+    return 264 * (destination - start);
+};
+
+function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+    if(distanceInFeet <= 400) {
+        return 0;
+    } else if(distanceInFeet > 400 && distanceInFeet <= 2000) {
+        return (distanceInFeet - 400) * 0.02;
+    } else if(distanceInFeet > 2000 && distanceInFeet <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+};
+
+console.log(calculatesFarePrice(43, 42));
